00b6406f3eb56a4edaf263856d363a9cce723914
nerda-nicxjo <83508158+nerda-nicxjo@users.noreply.github.com>
2021-05-14 15:31:35 -0500

    changed game height

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let canvas = document.getElementById("playField");
 let cx = canvas.getContext("2d");
 
 const GAME_WIDTH = 800;
-const GAME_HEIGHT = 800;
+const GAME_HEIGHT = 600;
 const PLAYER_SIZE = 40;
 
 let player = new Player("#0f0", PLAYER_SIZE, GAME_WIDTH / 2 - PLAYER_SIZE / 2 , GAME_HEIGHT / 2 - PLAYER_SIZE / 2);
